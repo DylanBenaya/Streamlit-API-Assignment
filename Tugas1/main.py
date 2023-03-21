a00dfd8d4f15181f5f44f6fc1d089fbd7afeffe8
Dylan Benaya <Dylan.benaya13@gmail.com>
2023-03-21 13:53:14 +0700

    Streamlit Assignment Update

--- Tugas1/main.py
+++ Tugas1/main.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-# Load the data
-data = pd.read_csv("datakependudukan.csv")
+def load_data(sheets_url):
+  csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+  return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
 
 # Set the page title
 st.set_page_config(page_title="Indonesian Population Data", page_icon=":bar_chart:")
@@ -13,15 +16,17 @@ st.header("Indonesian Population Data from 2015 to 2020")
 data_option = st.multiselect(
 
     'Pilih Daerah yang kamu mau',
-    ['ACEH', 'SUMATERA UTARA', 'SUMATERA BARAT', 'RIAU', 'JAMBI', 'SUMATERA SELATAN', 'BENGKULU', 'LAMPUNG', 
-    'KEP. BANGKA BELITUNG', 'KEP. RIAU', 'DKI JAKARTA', 'JAWA BARAT', 'JAWA TENGAH','DI YOGYAKARTA', 'JAWA TIMUR', 'BANTEN',
-    'BALI',' NUSA TENGGARA BARAT', 'NUSA TENGGARA TIMUR', 'KALIMANTAN BARAT', 'KALIMANTAN TENGAH', 'KALIMANTAN SELATAN', 
-    'KALIMANTAN TIMUR', 'KALIMANTAN UTARA',' SULAWESI UTARA', 'SULAWESI TENGAH', 'SULAWESI SELATAN', 'SULAWESI TENGGARA', 
-    'GORONTALO', 'SULAWESI BARAT', 'MALUKU', 'MALUKU UTARA', 'PAPUA BARAT' , 'PAPUA', 'INDONESIA'
+    ['ACEH', 'SUMATERA_UTARA', 'SUMATERA_BARAT', 'RIAU', 'JAMBI', 'SUMATERA_SELATAN', 'BENGKULU', 'LAMPUNG', 
+    'KEP_BANGKA BELITUNG', 'KEP_RIAU', 'DKI_JAKARTA', 'JAWA_BARAT', 'JAWA_TENGAH','DI_YOGYAKARTA', 'JAWA_TIMUR', 'BANTEN',
+    'BALI',' NUSA_TENGGARA_BARAT', 'NUSA_TENGGARA_TIMUR', 'KALIMANTAN_BARAT', 'KALIMANTAN_TENGAH', 'KALIMANTAN_SELATAN', 
+    'KALIMANTAN_TIMUR', 'KALIMANTAN_UTARA',' SULAWESI_UTARA', 'SULAWESI_TENGAH', 'SULAWESI_SELATAN', 'SULAWESI_TENGGARA', 
+    'GORONTALO', 'SULAWESI_BARAT', 'MALUKU', 'MALUKU_UTARA', 'PAPUA_BARAT' , 'PAPUA', 'INDONESIA'
 ]
 )
-data_chart = pd.DataFrame(data)
+data_chart = pd.DataFrame(df)
 st.checkbox("Use container width", value=False, key="use_container_width")
-st.dataframe(data_chart, use_container_width=st.session_state.use_container_width)
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
 st.line_chart(data_chart, x='TAHUN', y=data_option)
+
+
 
--- Tugas2/main.py
+++ Tugas2/main.py
@@ -2,40 +2,45 @@ import streamlit as st
 import pandas as pd
 
 tab1, tab2, tab3, tab4 = st.tabs(['Dataframe', 'Chart', 'Widget', 'Full Element'])
-data = pd.read_csv("datakependudukan.csv")
+
+def load_data(sheets_url):
+  csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+  return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
 
 with tab1:
     
     st.header("Indonesian Population Data from 2015 to 2020")
-    data_chart = pd.DataFrame(data)
+    data_chart = pd.DataFrame(df)
     st.checkbox("Use container width", value=False, key="use_container_width")    
     st.dataframe(data_chart, use_container_width=st.session_state.use_container_width)
 
 with tab2:
 
     st.header("Indonesian Population Data from 2015 to 2020")
-    data_chart = pd.DataFrame(data)
+    data_chart = pd.DataFrame(df)
     st.line_chart(data_chart, x='TAHUN', y='INDONESIA')
 
 with tab3:
     data_option = st.multiselect(
 
         'Pilih Daerah yang kamu mau',
-        ['ACEH', 'SUMATERA UTARA', 'SUMATERA BARAT', 'RIAU', 'JAMBI', 'SUMATERA SELATAN', 'BENGKULU', 'LAMPUNG', 
-        'KEP. BANGKA BELITUNG', 'KEP. RIAU', 'DKI JAKARTA', 'JAWA BARAT', 'JAWA TENGAH','DI YOGYAKARTA', 'JAWA TIMUR', 'BANTEN',
-        'BALI',' NUSA TENGGARA BARAT', 'NUSA TENGGARA TIMUR', 'KALIMANTAN BARAT', 'KALIMANTAN TENGAH', 'KALIMANTAN SELATAN', 
-        'KALIMANTAN TIMUR', 'KALIMANTAN UTARA',' SULAWESI UTARA', 'SULAWESI TENGAH', 'SULAWESI SELATAN', 'SULAWESI TENGGARA', 
-        'GORONTALO', 'SULAWESI BARAT', 'MALUKU', 'MALUKU UTARA', 'PAPUA BARAT' , 'PAPUA', 'INDONESIA'
+        ['ACEH', 'SUMATERA_UTARA', 'SUMATERA_BARAT', 'RIAU', 'JAMBI', 'SUMATERA_SELATAN', 'BENGKULU', 'LAMPUNG', 
+        'KEP_BANGKA BELITUNG', 'KEP_RIAU', 'DKI_JAKARTA', 'JAWA_BARAT', 'JAWA_TENGAH','DI_YOGYAKARTA', 'JAWA_TIMUR', 'BANTEN',
+        'BALI',' NUSA_TENGGARA_BARAT', 'NUSA_TENGGARA_TIMUR', 'KALIMANTAN_BARAT', 'KALIMANTAN_TENGAH', 'KALIMANTAN_SELATAN', 
+        'KALIMANTAN_TIMUR', 'KALIMANTAN_UTARA',' SULAWESI_UTARA', 'SULAWESI_TENGAH', 'SULAWESI_SELATAN', 'SULAWESI_TENGGARA', 
+        'GORONTALO', 'SULAWESI_BARAT', 'MALUKU', 'MALUKU_UTARA', 'PAPUA_BARAT' , 'PAPUA', 'INDONESIA'
     ]
     )
     st.header("Indonesian Population Data from 2015 to 2020")
-    data_chart = pd.DataFrame(data)
+    data_chart = pd.DataFrame(df)
     st.line_chart(data_chart, x='TAHUN', y=data_option)
 
 with tab4:
 
     st.header("Indonesian Population Data from 2015 to 2020")
-    data_chart = pd.DataFrame(data)
+    data_chart = pd.DataFrame(df)
     st.checkbox("Use container width", value=False, key="container_width")    
     st.dataframe(data_chart, use_container_width=st.session_state.container_width)
     st.line_chart(data_chart, x='TAHUN', y=data_option)
